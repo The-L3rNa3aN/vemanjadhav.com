86f201086364d72ddd197a7058777dd13d0e5a12
Veman Jadhav <vemancraft@gmail.com>
2026-07-23 00:19:09 +0530

    Added lateral player movement. Doesn't completely fall down for some reason.

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,65 @@
-export default class Player
+import RAPIER from "@dimforge/rapier3d";
+import * as THREE from "three";
+
+export class Player
 {
-    //Add stuff here.
+    public mesh: THREE.Mesh;
+    speed: number;
+    // public rigidbody: RAPIER.RigidBody;
+    keys: Record<string, boolean> = {};
+
+    constructor(private physics: any, scene: THREE.Scene, startPos: THREE.Vector3 = new THREE.Vector3(0, 2, 0))
+    {
+        let geometry = new THREE.CapsuleGeometry(0.5, 1, 4, 8);
+        let material = new THREE.MeshLambertMaterial({ color: 0x808080 });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.speed = 5;
+
+        this.mesh.position.copy(startPos);
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
+        scene.add(this.mesh);
+
+        // let rbDesc = RAPIER.RigidBodyDesc.kinematicPositionBased().setTranslation(startPos.x, startPos.y, startPos.z);
+        // this.rigidbody = this.world.createRigidBody(rbDesc);
+        // let collDesc = RAPIER.ColliderDesc.capsule(0.5, 0.4);
+        // this.world.createCollider(collDesc, this.rigidbody);
+
+        this.physics.addMesh(this.mesh, 1, 0.3);
+
+        //Input listeners (for now)
+        window.addEventListener('keydown', (e) => this.keys[e.key.toLowerCase()] = true);
+        window.addEventListener('keyup', (e) => this.keys[e.key.toLowerCase()] = false);
+    }
+
+    public update(delta: number)
+    {
+        if(!this.mesh || !this.physics) return;
+        
+        let moveDir = new THREE.Vector3();
+
+        if(this.keys['a']) moveDir.x -= 1;
+        if(this.keys['d']) moveDir.x += 1;
+
+        if(moveDir.length() > 0) moveDir.normalize();
+
+        let moveVector = moveDir.multiplyScalar(this.speed * delta);
+        // let currentPos = this.rigidbody.translation();
+        // let targetPos = {x: currentPos.x + moveVector.x, y: currentPos.y, z: currentPos.z};
+
+        // this.rigidbody.setNextKinematicTranslation(targetPos);
+
+        // let pos = this.rigidbody.translation();
+        // this.mesh.position.set(pos.x, pos.y, pos.z);
+
+        this.mesh.position.x += moveVector.x;
+        this.physics.setMeshPosition(this.mesh, this.mesh.position, true);
+    }
+
+    public dispose(/* world: RAPIER.World */)
+    {
+        window.removeEventListener('keydown', () => {});
+        window.removeEventListener('keyup', () => {});
+        // world.removeRigidBody(this.rigidbody);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { RapierPhysics } from 'three/examples/jsm/Addons.js';
+import { Player } from './Player';
+import RAPIER from '@dimforge/rapier3d';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -7,6 +9,7 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 const timer = new THREE.Timer();
 let physics: any;
+let world: RAPIER.World;
 
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -31,17 +34,18 @@ const cube = new THREE.Mesh(geometry, material);
 cube.castShadow = true;
 cube.receiveShadow = true;
 
-let capsGeo = new THREE.CapsuleGeometry(0.5, 1, 4, 8);
+/* let capsGeo = new THREE.CapsuleGeometry(0.5, 1, 4, 8);
 const capsMat = new THREE.MeshStandardMaterial({ color: 0x808080 });
 let caps = new THREE.Mesh(capsGeo, capsMat);
 caps.castShadow = true;
 caps.receiveShadow = true;
 caps.position.set(0, 2, 0);
-caps.rotation.z = 0.25;			//For the time being. Only for testing.
+caps.rotation.z = 0.25;	 */		//For the time being. Only for testing.
 
 camera.position.set(0, 1, 5);
 
-scene.add(cube, caps, dirLight);
+// scene.add(cube, caps, dirLight);
+scene.add(cube, dirLight);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
@@ -54,11 +58,18 @@ window.addEventListener('resize', () =>
 	renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-async function initPhysics() { physics = await RapierPhysics(); }
+async function initPhysics()
+{
+	physics = await RapierPhysics();
+	// world = physics.world;
+}
 await initPhysics();
+// await new Promise(resolve => setTimeout(resolve, 1000));
+
+let player = new Player(physics, scene, new THREE.Vector3(0, 2, 0));
 
 //Adding physics to capsule.
-physics.addMesh(caps, 1, 0.3);
+// physics.addMesh(caps, 1, 0.3);
 physics.addMesh(cube, 0);
 
 function updateLoop(timestamp)
@@ -69,12 +80,12 @@ function updateLoop(timestamp)
 
 	const delta = timer.getDelta();
 
-	// Enabling this freezes all physics and I don't know why.
-	// if(physics)
-	// {
-	// 	physics.world.step();
-	// 	physics.update();
-	// }
+	if(physics)
+	{
+		physics.world.step();
+	}
+
+	player.update(delta);
 
 	renderer.render(scene, camera);
 }
